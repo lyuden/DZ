65e91d5e975e3707a831e31416b2bcc70fbfde8d
AnikinMaxim <mi.ego.velichestvo@gmail.com>
2017-03-30 11:04:08 +0700

    Use encoding - latin-1

--- MyHomeWorkCode.py
+++ MyHomeWorkCode.py
@@ -1,11 +1,17 @@
 # 1st and 2nd
-with open('str.txt', 'w', encoding='utf-8') as myfile:
-    myfile.write('Чето там пишем и че то там выводим')
+# with open('str.txt', 'w', encoding='utf-8') as myfile:
+#     myfile.write('Чето там пишем и че то там выводим')
 
 # write-mode - not deadable
 # with open('str.txt', 'r', encoding='utf-8') as myfile:
 #     print(myfile.read())
 
 # read as bite
-with open('str.txt', 'rb') as myfile:
+# with open('str.txt', 'rb') as myfile:
+#     print(myfile.read())
+
+with open('str.txt', 'w', encoding='latin-1') as myfile:
+    myfile.write('more short')
+
+with open('str.txt', 'r', encoding='latin-1') as myfile:
     print(myfile.read())
