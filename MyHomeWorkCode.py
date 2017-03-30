9236a739995cfe25842c20c3a1cc7d2cc9a56433
AnikinMaxim <mi.ego.velichestvo@gmail.com>
2017-03-30 11:56:39 +0700

    decode last file in bite

--- MyHomeWorkCode.py
+++ MyHomeWorkCode.py
@@ -10,8 +10,16 @@
 # with open('str.txt', 'rb') as myfile:
 #     print(myfile.read())
 
-with open('str.txt', 'w', encoding='latin-1') as myfile:
-    myfile.write('more short')
+# with open('str.txt', 'w', encoding='latin-1') as myfile:
+#     myfile.write('Зачем то что то пишу опять')
+#
+# with open('str.txt', 'r', encoding='latin-1') as myfile:
+#     print(myfile.read())
 
-with open('str.txt', 'r', encoding='latin-1') as myfile:
+# Correctly text in file for latin-1
+with open('str.txt', 'w', encoding='latin-1') as myfile:
+    myfile.write('any text for this encoding')
+
+# decode last file in bite
+with open('str.txt', 'br') as myfile:
     print(myfile.read())
