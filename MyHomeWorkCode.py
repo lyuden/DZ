851aced91dc37cf9139ad3eec67af38874f5be33
AnikinMaxim <mi.ego.velichestvo@gmail.com>
2017-03-30 10:55:54 +0700

    New feature - read txt as bite

--- MyHomeWorkCode.py
+++ MyHomeWorkCode.py
@@ -1,6 +1,11 @@
+# 1st and 2nd
 with open('str.txt', 'w', encoding='utf-8') as myfile:
-    myfile.write('mystring')
+    myfile.write('Чето там пишем и че то там выводим')
 
 # write-mode - not deadable
-with open('str.txt', 'r', encoding='utf-8') as myfile:
+# with open('str.txt', 'r', encoding='utf-8') as myfile:
+#     print(myfile.read())
+
+# read as bite
+with open('str.txt', 'rb') as myfile:
     print(myfile.read())
